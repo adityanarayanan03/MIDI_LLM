e2799336fa418f2eba8f3f2a2412445b855ecc98
Aditya Narayanan <aditya.narayanan03@gmail.com>
2023-11-30 23:18:39 -0600

    packaged it nicely so I can import my functions elsewhere

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-from inference import LLM
+'''
+Main run code. Some development done in jupyter notebooks to make things 
+easier for now with loading datasets, but it should get populated here for 
+the final product.
+'''
 
-llm = LLM("config.yaml")
-
-#Mock scenario -- generating only one token at a time
-context = "I would walk 500 miles and "
-for _ in range(100):
-    text, tokens = llm.infer(context)
-    context += text
-    print(context)
